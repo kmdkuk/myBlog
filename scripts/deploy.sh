04287c2832bd6a84be2c2f8dc0027c97315917bc
kouki <kouworld0123@gmail.com>
2020-08-14 16:38:53 +0900

    fix: shallow update not allowed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 echo "deploy start"
 git config core.sshCommand 'ssh -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+git fetch --unshallow
+git pull origin master
 echo "DEPLOY TO $DEPLOY_HOST"
 git remote add deploy $DEPLOY_HOST
 git push -f deploy master
